af7cc9a05c306d6f4b1afe23a92c3af1b2ca791b
Галина Горбель <galinagorbel@gmail.com>
2023-01-27 11:15:20 +0300

    создана функция trimSymbols

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,8 +4,5 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (!obj) {
-    return;
-  }
-  return Object.fromEntries(Object.entries(obj).map((e) => e.reverse()));
+  return !obj ? undefined : Object.fromEntries(Object.entries(obj).map((e) => e.reverse()));
 }
